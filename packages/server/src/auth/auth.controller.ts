70c1c0d6c3cdc44b4198ad45b849d8b8e5183658
manandesai54 <manan5401desai@gmail.com>
2025-06-30 17:52:03 +0530

    me call

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Inject, Post, ValidationPipe } from '@nestjs/common';
-import { LoginUserDto } from './dto/auth.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthContextType, LoginUserDto } from './dto/auth.dto';
+import { AuthContext } from './decorators/authContext';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +19,11 @@ export class AuthController {
   @Post('/login')
   login(@Body(ValidationPipe) loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/me')
+  me(@AuthContext() ctx: AuthContextType) {
+    return this.authService.me(ctx);
   }
 }
--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -1,41 +1,49 @@
 import {
   Inject,
   Injectable,
-  Logger,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
-import { LoginSuccessDto, LoginUserDto } from './dto/auth.dto';
 import { EncryptionUtil } from 'src/common/utils/encryptionUtil';
 import { JWTUtils } from 'src/common/utils/jwtUtil';
+import { UserService } from 'src/user/user.service';
+import { AuthContextType, LoginSuccessDto, LoginUserDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor(@Inject() private readonly userService: UserService) {}
 
   async login(loginUserDto: LoginUserDto): Promise<LoginSuccessDto> {
-    const user = await this.userService.getUserForAuthentication(
-      loginUserDto.email,
-    );
-    if (!user) {
-      throw new NotFoundException('User not found');
+    try {
+      const user = await this.userService.getUserForAuthentication(
+        loginUserDto.email,
+      );
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+      const isPasswordValid = await EncryptionUtil.compareHashedPassword(
+        loginUserDto.password,
+        user.password,
+      );
+      if (!isPasswordValid) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      const userJwtToken = await JWTUtils.generateToken({
+        email: user.email,
+        userId: user._id,
+      });
+      return {
+        token: userJwtToken,
+        email: user.email,
+        userId: user._id,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-    const isPasswordValid = await EncryptionUtil.compareHashedPassword(
-      loginUserDto.password,
-      user.password,
-    );
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-    const userJwtToken = await JWTUtils.generateToken({
-      email: user.email,
-      userId: user._id,
-    });
-    return {
-      token: userJwtToken,
-      email: user.email,
-      userId: user._id,
-    };
+  }
+
+  async me(authContext: AuthContextType) {
+    return this.userService.getUserById(authContext.userId);
   }
 }
