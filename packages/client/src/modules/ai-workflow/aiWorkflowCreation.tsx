d429cd2430d64f227f21f23262e676098860f464
manandesai54 <manan5401desai@gmail.com>
2025-07-14 07:53:22 +0530

    ai eorflow creation

--- packages/client/src/modules/ai-workflow/aiWorkflowCreation.tsx
+++ packages/client/src/modules/ai-workflow/aiWorkflowCreation.tsx
@@ -4,27 +4,28 @@ import {
 } from "@gryffindor/client/common/api/decorators/hoc/themeProvider";
 import { EMPTY_ARRAY_READ_ONLY } from "@gryffindor/client/common/constants/readOnly";
 import {
-  addEdge,
-  applyEdgeChanges,
-  applyNodeChanges,
-  Background,
-  Controls,
-  Edge,
-  MiniMap,
-  Node,
-  OnConnect,
-  OnEdgesChange,
-  OnNodesChange,
-  ReactFlow,
-} from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
-import { useCallback, useContext, useState } from "react";
-import { SettingsPanel } from "./components/settingsPanel";
-import {
   AiWorkflowComponentCategory,
   AiWorkflowComponentType,
   BaseWorkflowComponent,
 } from "@gryffindor/client/common/types/ai-workflow/ai-workflow.type";
+import {
+  addEdge,
+  Background,
+  Controls,
+  MiniMap,
+  NodeTypes,
+  OnConnect,
+  ReactFlow,
+  ReactFlowProvider,
+  useEdgesState,
+  useNodesState,
+  useReactFlow,
+  XYPosition,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { DragEventHandler, useCallback, useContext, useRef } from "react";
+import { SettingsPanel } from "./components/settingsPanel";
+import WorkflowInputNode from "./components/workflowInputNode";
 
 const WorkflowComponents: Record<
   AiWorkflowComponentCategory,
@@ -61,32 +62,90 @@ const WorkflowComponents: Record<
   [AiWorkflowComponentCategory.Data]: [],
 };
 
-export default function AiWorkflowCreation() {
-  const [nodes, setNodes] = useState<Node[]>(EMPTY_ARRAY_READ_ONLY);
-  const [edges, setEdges] = useState<Edge[]>(EMPTY_ARRAY_READ_ONLY);
+const NODE_TYPES: NodeTypes = {
+  "workflow-node": WorkflowInputNode,
+};
+
+function WorkflowCreation() {
+  const reactFlowRef = useRef<HTMLDivElement>(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(EMPTY_ARRAY_READ_ONLY);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(EMPTY_ARRAY_READ_ONLY);
+  const { screenToFlowPosition } = useReactFlow();
+
   const { theme } = useContext(ThemeContext);
 
-  const onNodesChange: OnNodesChange = useCallback(
-    (changes) =>
-      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-    [],
-  );
-  const onEdgesChange: OnEdgesChange = useCallback(
-    (changes) =>
-      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-    [],
-  );
   const onConnect: OnConnect = useCallback(
     (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
-    [],
+    [setEdges],
   );
+
+  const appendNode = useCallback(
+    (position: XYPosition, component: BaseWorkflowComponent) => {
+      setNodes((nodes) => [
+        ...nodes,
+        {
+          id: `${component.type}-${Date.now()}`,
+          position,
+          data: {
+            ...component,
+            id: `${component.type}-${Date.now()}`,
+          },
+          type: "workflow-node",
+        },
+      ]);
+    },
+    [setNodes],
+  );
+
+  const onAddComponent = useCallback(
+    (component: BaseWorkflowComponent) => {
+      const position = screenToFlowPosition({
+        x: reactFlowRef.current?.clientWidth
+          ? reactFlowRef.current.clientWidth / 2
+          : 0,
+        y: reactFlowRef.current?.clientHeight
+          ? reactFlowRef.current.clientHeight / 2
+          : 0,
+      });
+      appendNode(position, component);
+    },
+    [screenToFlowPosition, appendNode],
+  );
+
+  const onDrop: DragEventHandler<HTMLDivElement> = useCallback(
+    (event) => {
+      event.preventDefault();
+      const componentJson = event.dataTransfer.getData("application/reactflow");
+      const component = JSON.parse(componentJson) as BaseWorkflowComponent;
+
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+
+      appendNode(position, component);
+    },
+    [screenToFlowPosition, appendNode],
+  );
+
+  const onDragOver: DragEventHandler<HTMLDivElement> = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.effectAllowed = "move";
+  }, []);
 
   return (
     <div className="w-full h-full bg-secondary flex">
-      <SettingsPanel workflowComponents={WorkflowComponents} />
+      <SettingsPanel
+        workflowComponents={WorkflowComponents}
+        onAddComponent={onAddComponent}
+      />
       <ReactFlow
+        nodeTypes={NODE_TYPES}
+        ref={reactFlowRef}
         nodes={nodes}
         edges={edges}
+        onDrop={onDrop}
+        onDragOver={onDragOver}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
@@ -97,5 +156,13 @@ export default function AiWorkflowCreation() {
         <MiniMap />
       </ReactFlow>
     </div>
+  );
+}
+
+export default function AiWorkflowCreation() {
+  return (
+    <ReactFlowProvider>
+      <WorkflowCreation />
+    </ReactFlowProvider>
   );
 }
---
+++ packages/client/src/modules/ai-workflow/components/workflowInputNode.tsx
@@ -0,0 +1,26 @@
+import AppCard from "@gryffindor/client/common/components/app/appCard/appCard";
+import { AiWorkflowComponent } from "@gryffindor/client/common/types/ai-workflow/ai-workflow.type";
+import { Handle, NodeProps, Position } from "@xyflow/react";
+import { memo } from "react";
+
+function WorkflowInputNode(props: NodeProps<AiWorkflowComponent>) {
+  const { data } = props;
+
+  return (
+    <div className="w-[400px]">
+      <AppCard
+        title={data.name}
+        description={data.description}
+        content={<div className="bg-background">Input</div>}
+        footer={
+          <div className=" w-full">
+            Output
+            <Handle type="source" position={Position.Right} className="p-1" />
+          </div>
+        }
+      />
+    </div>
+  );
+}
+
+export default memo(WorkflowInputNode);
